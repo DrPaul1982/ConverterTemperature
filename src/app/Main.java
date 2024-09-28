fc2f46850cf4b765b3fc68e078565609beada80d
DrPaul1982 <avtoklavir@ukr.net>
2024-09-28 15:17:58 +0300

    Add convertion Fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,20 @@
 package src.app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Application for converting degrees Celsius to Fahrenheit and vice versa (Fahrenheit to Celsius).");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, input degrees in Fahrenheit: ");
+        double fahr = scanner.nextDouble();
+        System.out.println(fahr + " degrees Fahrenheit will be " + convFahrToCels(fahr) + " degrees Celsius.");
     }
+
+    private static double convFahrToCels(double fahr) {
+        return (fahr - 32) * 5 / 9;
+    }
+
 }
